7850c7d4153c0e7459839227f93389683fb66306
Xenia <lommy101@gmail.com>
2020-03-09 00:24:39 +0900

    add graph

--- CoronaD.py
+++ CoronaD.py
@@ -3,9 +3,17 @@ import getData
 import datetime as dt
 import pandas as pd
 
-data = getData.read_csv('list')
-df = pd.DataFrame(data, columns = ['date' , 'value'])
-df.set_index('date', inplace=True)
+def ShowGraph(data):
+    df = pd.DataFrame(data, columns = ['date' , 'value'])
+    df.date = pd.to_datetime(df.date)
+    df.value = pd.to_numeric(df['value'])
+    df = df.set_index('date')
+    g = df['value'].plot(title="Corona-Virus Daily data")
+    plt = g.get_figure()
+    plt.show()
 
-plt.plot(df.index, df.value, marker='s', color='r')
-plt.show()
+if __name__ == "__main__":
+    data = getData.read_csv('list')
+    ShowGraph(data)
+
+#https://byeongkijeong.github.io/ARIMA-with-Python/
