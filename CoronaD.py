0e8dd4de7dd741dcd4fb159785b95f0c9368e483
Xenia <lommy101@gmail.com>
2020-03-10 11:53:55 +0900

    add ACF & PACF graphs

--- CoronaD.py
+++ CoronaD.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import getData
 import datetime as dt
 import pandas as pd
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
-def ShowGraph(data):
+def ShowGraph(data): # Show data Graph
     df = pd.DataFrame(data, columns = ['date' , 'value'])
     df.date = pd.to_datetime(df.date)
     df.value = pd.to_numeric(df['value'])
@@ -12,8 +13,15 @@ def ShowGraph(data):
     plt = g.get_figure()
     plt.show()
 
+def Calc_ACF_PAF(): # ACF PAF Calculation with ARIMA models
+    data = pd.read_csv('./data/data.csv', header=0, index_col=0, squeeze=True)
+    plot_acf(data)
+    plot_pacf(data)
+    plt.show()
+    
 if __name__ == "__main__":
     data = getData.read_csv('list')
-    ShowGraph(data)
+    #ShowGraph(data)
+    Calc_ACF_PAF()
 
 #https://byeongkijeong.github.io/ARIMA-with-Python/
